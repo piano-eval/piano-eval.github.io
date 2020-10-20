f358283578ba21b349d17d38e00a306ba91f2c43
Johan Pauwels <jpauwels@users.noreply.github.com>
2020-10-20 10:46:47 +0100

    Simplify JSON sent to backend

--- service/write.php
+++ service/write.php
@@ -36,15 +36,14 @@ $filepathPostfix = ".csv";
 if (!is_dir($filepathPrefix)) {
     mkdir($filepathPrefix);
 }
-$length = count($session->participant->name);
 // mushra
 $write_mushra = false;
 $mushraCsvData = array();
 
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-	array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+	array_push($input, $key);
 }
 array_push($input, "session_uuid", "trial_id", "rating_stimulus", "rating_score", "rating_time", "rating_comment");
 array_push($mushraCsvData, $input);
@@ -59,8 +58,8 @@ array_push($mushraCsvData, $input);
 
 
     $results = array($session->testId);
-    for($i =0; $i < $length; $i++){
-      array_push($results, $session->participant->response[$i]);
+    foreach ($session->participant as $key => $value) {
+        array_push($results, $value);
     }
     array_push($results, $session->uuid, $trial->id, $response->stimulus, $response->score, $response->time, $response->comment);
 
@@ -95,8 +94,8 @@ $pcCsvData = array();
 // array_push($pcCsvData, array("session_test_id", "participant_email", "participant_age", "participant_gender", "trial_id", "choice_reference", "choice_non_reference", "choice_answer", "choice_time", "choice_comment"));
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-	array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+	array_push($input, $key);
 }
 array_push($input, "trial_id", "choice_reference", "choice_non_reference", "choice_answer", "choice_time", "choice_comment");
 array_push($pcCsvData, $input);
@@ -110,8 +109,8 @@ foreach ($session->trials as $trial) {
 		  
 		 
 		$results = array($session->testId);
-		for($i =0; $i < $length; $i++){
-			array_push($results, $session->participant->response[$i]);
+		foreach ($session->participant as $key => $value) {
+			array_push($results, $value);
 		}  
 		array_push($results, $trial->id, $response->reference, $response->nonReference, $response->answer, $response->time, $response->comment);
 	  
@@ -143,8 +142,8 @@ $write_bs1116 = false;
 $bs1116CsvData = array();
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-	array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+	array_push($input, $key);
 }
 array_push($input,  "trial_id", "rating_reference", "rating_non_reference", "rating_reference_score", "rating_non_reference_score", "rating_time", "choice_comment");
 array_push($bs1116CsvData, $input);
@@ -156,8 +155,8 @@ foreach ($session->trials as $trial) {
 	  	$write_bs1116 = true;
 		  
 		$results = array($session->testId);
-		for($i =0; $i < $length; $i++){
-			array_push($results, $session->participant->response[$i]);
+		foreach ($session->participant as $key => $value) {
+			array_push($results, $value);
 		}  
 		array_push($results, $trial->id, $response->reference, $response->nonReference, $response->referenceScore, $response->nonReferenceScore, $response->time, $response->comment);
 	  
@@ -189,8 +188,8 @@ $lmsCSVdata = array();
 // array_push($lmsCSVdata, array("session_test_id", "participant_email", "participant_age", "participant_gender", "trial_id", "stimuli_rating", "stimuli", "rating_time"));
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-	array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+	array_push($input, $key);
 }
 array_push($input,  "trial_id", "stimuli_rating", "stimuli", "rating_time");
 array_push($lmsCSVdata, $input);
@@ -202,8 +201,8 @@ foreach($session->trials as $trial) {
 			$write_lms = true; 
 			
 			$results = array($session->testId);
-			for($i =0; $i < $length; $i++){
-				array_push($results, $session->participant->response[$i]);
+			foreach ($session->participant as $key => $value) {
+				array_push($results, $value);
 			}  
 			array_push($results,  $trial->id, " $response->stimulusRating ", $response->stimulus, $response->time);
 		  
@@ -237,8 +236,8 @@ $lssCSVdata = array();
 // array_push($lssCSVdata, array("session_test_id", "participant_email", "participant_age", "participant_gender", "trial_id", "stimuli_rating", "stimuli", "rating_time"));
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-	array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+	array_push($input, $key);
 }
 array_push($input,  "trial_id");
 $ratingCount = count($session->trials[0]->responses[0]->stimulusRating);
@@ -259,8 +258,8 @@ foreach($session->trials as $trial) {
 			$write_lss = true; 
 			
 				$results = array($session->testId);
-			for($i =0; $i < $length; $i++){
-				array_push($results, $session->participant->response[$i]);
+			foreach ($session->participant as $key => $value) {
+				array_push($results, $value);
 			}
             array_push($results, $trial->id);
             $results = array_merge($results, $response->stimulusRating);
@@ -293,8 +292,8 @@ $write_spatial_localization = false;
 $spatial_localizationData = array();
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-    array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+    array_push($input, $key);
 }
 array_push($input,  "trial_id", "name", "stimulus", "position_x", "position_y", "position_z");
 array_push($spatial_localizationData, $input);
@@ -309,8 +308,8 @@ foreach ($session->trials as $trial) {
         $write_spatial_localization = true;
     
         $results = array($session->testId);
-        for($i =0; $i < $length; $i++){
-            array_push($results, $session->participant->response[$i]);
+        foreach ($session->participant as $key => $value) {
+            array_push($results, $value);
         }  
 array_push($results, $trial->id, $response->name, $response->stimulus, $response->position[0], $response->position[1], $response->position[2]);
       
@@ -341,8 +340,8 @@ $write_spatial_asw = false;
 $spatial_aswData = array();
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-    array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+    array_push($input, $key);
 }
 
 array_push($input,  "trial_id", "name", "stimulus", "position_outerRight_x", "position_outerRight_y", "position_outerRight_z", "position_innerRight_x", "position_innerRight_y", "position_innerRight_z", "position_innerLeft_x", "position_innerLeft_y", "position_innerLeft_z", "position_outerLeft_x", "position_outerLeft_y", "position_outerLeft_z");
@@ -358,8 +357,8 @@ foreach ($session->trials as $trial) {
         $write_spatial_asw = true;
     
         $results = array($session->testId);
-        for($i =0; $i < $length; $i++){
-            array_push($results, $session->participant->response[$i]);
+        foreach ($session->participant as $key => $value) {
+            array_push($results, $value);
         }  
                 array_push($results, $trial->id, $response->name, $response->stimulus, $response->position_outerRight[0], $response->position_outerRight[1], $response->position_outerRight[2], $response->position_innerRight[0], $response->position_innerRight[1], $response->position_innerRight[2], $response->position_innerLeft[0], $response->position_innerLeft[1], $response->position_innerLeft[2], $response->position_outerLeft[0], $response->position_outerLeft[1], $response->position_outerLeft[2]);
         
@@ -390,8 +389,8 @@ $write_spatial_hwd = false;
 $spatial_hwdData = array();
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-    array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+    array_push($input, $key);
 }
 
 array_push($input,  "trial_id", "name", "stimulus", "position_outerRight_x", "position_outerRight_y", "position_outerRight_z", "position_innerRight_x", "position_innerRight_y", "position_innerRight_z", "position_innerLeft_x", "position_innerLeft_y", "position_innerLeft_z", "position_outerLeft_x", "position_outerLeft_y", "position_outerLeft_z", "height", "depth");
@@ -407,8 +406,8 @@ foreach ($session->trials as $trial) {
         $write_spatial_hwd = true;
     
         $results = array($session->testId);
-        for($i =0; $i < $length; $i++){
-            array_push($results, $session->participant->response[$i]);
+        foreach ($session->participant as $key => $value) {
+            array_push($results, $value);
         }  
                 array_push($results, $trial->id, $response->name, $response->stimulus, $response->position_outerRight[0], $response->position_outerRight[1], $response->position_outerRight[2], $response->position_innerRight[0], $response->position_innerRight[1], $response->position_innerRight[2], $response->position_innerLeft[0], $response->position_innerLeft[1], $response->position_innerLeft[2], $response->position_outerLeft[0], $response->position_outerLeft[1], $response->position_outerLeft[2], $response->height, $response->depth);
         
@@ -438,8 +437,8 @@ $write_spatial_lev = false;
 $spatial_levData = array();
 
 $input = array("session_test_id");
-for($i =0; $i < $length; $i++){
-    array_push($input, $session->participant->name[$i]);
+foreach ($session->participant as $key => $value) {
+    array_push($input, $key);
 }
 
 array_push($input,  "trial_id", "name", "stimulus", "position_center_x", "position_center_y", "position_center_z", "position_height_x", "position_height_y", "position_height_z", "position_width1_x", "position_width1_y", "position_width1_z", "position_width2_x", "position_width2_y", "position_width2_z");
@@ -455,8 +454,8 @@ foreach ($session->trials as $trial) {
         $write_spatial_lev = true;
     
         $results = array($session->testId);
-        for($i =0; $i < $length; $i++){
-            array_push($results, $session->participant->response[$i]);
+        foreach ($session->participant as $key => $value) {
+            array_push($results, $value);
         }  
         array_push($results, $trial->id, $response->name, $response->stimulus, $response->position_center[0], $response->position_center[1], $response->position_center[2], $response->position_height[0], $response->position_height[1], $response->position_height[2], $response->position_width1[0], $response->position_width1[1], $response->position_width1[2], $response->position_width2[0], $response->position_width2[1], $response->position_width2[2]);
         
