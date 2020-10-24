a803462805a414e92d22fa218e2cb56614ebb716
Johan Pauwels <jpauwels@users.noreply.github.com>
2020-10-24 21:48:27 +0100

    Allow adding questionnaires to GenericPages

--- startup.js
+++ startup.js
@@ -76,7 +76,7 @@ function addPagesToPageManager(_pageManager, _pages) {
     } else {
       var pageConfig = _pages[i];
       if (pageConfig.type == "generic") {
-        _pageManager.addPage(new GenericPage(_pageManager, pageConfig));
+        _pageManager.addPage(new GenericPage(_pageManager, pageTemplateRenderer, pageConfig, session, config.language));
       } else if (pageConfig.type == "consent") {
         _pageManager.addPage(new ConsentPage(_pageManager, pageTemplateRenderer, pageConfig));
       } else if (pageConfig.type == "volume") {
@@ -103,7 +103,7 @@ function addPagesToPageManager(_pageManager, _pages) {
         var likertMultiStimulusPage = new LikertMultiStimulusPage(pageManager, pageTemplateRenderer, pageConfig, audioContext, config.bufferSize, audioFileLoader, session, errorHandler, config.language);
         _pageManager.addPage(likertMultiStimulusPage);
       } else if (pageConfig.type == "finish") {
-        var finishPage = new FinishPage(_pageManager, session, dataSender, pageConfig, config.language);
+        var finishPage = new FinishPage(_pageManager, pageTemplateRenderer, pageConfig, session, config.language, dataSender);
         _pageManager.addPage(finishPage);
       } else {
 
